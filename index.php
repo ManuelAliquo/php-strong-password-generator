3b50e8aa3890fe688f7f808bb7d889880e0362fd
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-04-30 18:39:50 +0200

    add style

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             $password = generatePassword($chosenlength);
             $_SESSION["password"] = $password;
             header("Location: result.php");
-        } else $errorMessage = "Invalid length";
+        } else $errorMessage = "Invalid length, must be between 8 and 20!";
     }
 ?>
 
@@ -24,27 +24,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+    crossorigin="anonymous">
     <title>Password Generator</title>
 </head>
-<body>
-    <header>
-        <h1>Password Generator</h1>
-    </header>
-    <main>
-        <form action="">
-            <label for="pass-length">Password length</label>
-            <input
-            type="number"
-            id="pass-length"
-            name="pass-length"
-            required
-            min="<?php echo $minLength ?>"
-            max="<?php echo $maxLength ?>">
-            <button>Generate</button>
-        </form>
-        <div>
-            <?php if ($errorMessage !== "") echo $errorMessage?>
+
+<body class="bg-dark">
+    <div class="container mt-5 p-5">
+        <div class="w-50 mx-auto border rounded-4 p-5 bg-light">
+            <h1 class="text-center mb-4">Password Generator</h1>
+            <form action="">
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Password length</span>
+                    <input
+                    type="number"
+                    class="form-control"
+                    id="pass-length"
+                    name="pass-length"
+                    required
+                    placeholder="<?php
+                    if ($errorMessage == "") echo "Must be between 8 and 20";
+                    else echo $errorMessage?>">
+                </div>
+                <div class="d-flex justify-content-between align-items-end">
+                    <!-- CHECKS -->
+                    <div class="fs-5">
+                        <div class="form-check">
+                            <label class="form-check-label" for="letters-check">
+                                Letters
+                            </label>
+                            <input class="form-check-input" type="checkbox" value="" id="letters-check">
+                        </div>
+                        <div class="form-check">
+                            <label class="form-check-label" for="numbers-check">
+                                Numbers
+                            </label>
+                            <input class="form-check-input" type="checkbox" value="" id="numbers-check">
+                        </div>
+                        <div class="form-check">
+                            <label class="form-check-label" for="symbols-check">
+                                Symbols
+                            </label>
+                            <input class="form-check-input" type="checkbox" value="" id="symbols-check">
+                        </div>
+                    </div>
+                    <!-- SUBMIT -->
+                    <button class="btn btn-outline-primary mt-3">Generate</button>
+                </div>
+            </form>
         </div>
-    </main>
+    </div>
 </body>
 </html>
--- result.php
+++ result.php
@@ -5,15 +5,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+    crossorigin="anonymous">
     <title>Your Password</title>
 </head>
-<body>
-    <header>
-        <h1>Your Password</h1>
-    </header>
-    <main>
-        <div> Your Password: <?php echo $_SESSION["password"]?></div>
-        <a href="index.php">Go Back</a>
-    </main>
+
+<body class="bg-dark">
+    <div class="container mt-5 p-5">
+        <div class="w-50 mx-auto border rounded-4 p-5 bg-light text-center">
+            <h1 class="mb-4">Your Password</h1>
+    
+            <div class="mb-3"> Your Password is: <b><?php echo $_SESSION["password"]?></b></div>
+            <a class="btn btn-outline-secondary text-decoration-none text-black" href="index.php">Go Back</a>
+        </div>
+    </div>
 </body>
 </html>
